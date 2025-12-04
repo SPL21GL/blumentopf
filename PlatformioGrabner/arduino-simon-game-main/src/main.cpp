83830f8c294b086d36f6fe54440f8886ec7daad6
HGrabner <gl@bulme.at>
2025-12-04 16:09:48 +0100

    add new functionality

--- PlatformioGrabner/arduino-simon-game-main/src/main.cpp
+++ PlatformioGrabner/arduino-simon-game-main/src/main.cpp
@@ -1,33 +1,53 @@
 #include <Arduino.h>
+//Bounce 2 Library importieren
+#include <Bounce2.h>
+
+//Buttons an den Pins 2 und 3
+//#define BTN_YLW 2
+//#define BTN_BLU 3
+
+const int BTN_YLW = 2;
+const int BTN_BLU = 3;
+
+const int LED_GRN = 8;
+const int LED_PNK = 9;
+
+Bounce2::Button btnYellow = Bounce2::Button();
+Bounce2::Button btnBlue = Bounce2::Button();
 
 int counter = 0;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(LED_GRN,OUTPUT);
+  pinMode(LED_PNK,OUTPUT);
+
+  btnYellow.attach(BTN_YLW, INPUT_PULLUP); //Hier Button auf Pin 2 zuordnen
+  btnYellow.setPressedState(LOW); //Button ist Active-Low
+  
+  btnBlue.attach(BTN_BLU, INPUT_PULLUP);
+  btnBlue.setPressedState(LOW);
 }
 
 void loop() {
-  counter++;
+  btnYellow.update();
+  btnBlue.update();
 
-  if (counter % 3 == 0)
+  if (btnYellow.pressed())
   {
-    if (counter % 5 == 0) {
-      Serial.println("Fizz Buzz");
-    }
-    else {
-      Serial.println("Fizz");
-    }
+    Serial.println("Yellow pressed");
+
+    //Naiver Ansatz --> wird später verbessert (entfernt)
+    digitalWrite(LED_GRN,HIGH);
+    delay(2000);
+    digitalWrite(LED_GRN,LOW);
   }
-  else
+  
+  if (btnBlue.pressed())
   {
-    if (counter % 5 == 0) {
-      Serial.println("Buzz");
-    }
-    else {
-      Serial.println(counter);
-    }
+    Serial.println("Blue pressed");
   }
 
-  delay(500);
 }
 
